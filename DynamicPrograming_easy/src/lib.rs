76e6a13ab6f4f49441ba07890c1427464455d522
Chenjunjie1996 <74704889+Chenjunjie1996@users.noreply.github.com>
2023-02-27 17:00:26 +0800

    Update lib.rs

--- DynamicPrograming_easy/src/lib.rs
+++ DynamicPrograming_easy/src/lib.rs
@@ -116,4 +116,120 @@ impl Solution {
         }
         dp[len-1]
     }
+
+    pub fn min_cost_climbing_stairs(mut cost: Vec<i32>) -> i32 {
+        /*
+        给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+        你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+        输入：cost = [10,15,20]
+        输出：15
+        解释：你将从下标为 1 的台阶开始。
+        - 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+        总花费为 15 。
+        */
+        let n =cost.len();
+        let mut dp = vec![0; n+1];
+        for i in 2..=n {
+            dp[i] = (dp[i-1] + cost[i-1]).min(dp[i-2] + cost[i-2]);
+        }
+        dp[n]
+    }
+
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        /* 买卖股票最佳时机
+        输入：[7,1,5,3,6,4]
+        输出：5
+        解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出
+        输入：[7,6,4,3,1]
+        输出：0
+        */
+        let mut ans = 0;
+        let mut min_price = i32::MAX;
+        for price in prices.into_iter() {
+            if price < min_price {
+                min_price = price;
+            }
+            ans = ans.max(price-min_price)
+        }
+        ans
+    }
+
+    pub fn find_length_of_lcis(nums: Vec<i32>) -> i32 {
+        /*
+        // 输入: [1,3,5,4,7]
+        // 输出: 3
+        // 解释: 最长连续递增序列是 [1,3,5], 长度为3。
+        // 尽管 [1,3,5,7] 也是升序的子序列, 但它不是连续的，因为5和7在原数组里被4隔开。
+        */
+        // let len = nums.len();
+        // let mut max_len:i32 = 0;
+        // let mut sub_nums: Vec<i32> = vec![];
+        // for i in 0..len {
+        //     sub_nums.push(nums[i]);
+        //     if nums[i+1] < nums[i] {
+        //         sub_nums.push(nums[i+1])
+        //    } else {
+        //         max_len = max_len.max(sub_nums.len() as i32);
+        //         sub_nums = vec![];
+        //     }
+        // }
+        // max_len
+        if nums.len() <= 1 {
+            return nums.len() as i32;
+        }
+        let mut res = 1;
+        let mut path = vec![1; nums.len()];
+        for i in 1..nums.len() {
+            if nums[i] > nums[i-1] {
+                path[i] = path[i-1] + 1;
+                res = max(res, path[i]);
+            }
+        }
+        res
+    }
+
+    pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
+        /*有序数组的平方
+        输入：nums = [-7,-3,2,3,11]
+        输出：[4,9,9,49,121] 
+        */
+        // 1.直接排序
+        // let res = nums.into_iter().map(|x| x.pow(2)).collect();
+        // res
+        // 2.双指针
+        let mut dp = vec![];
+        let mut start = 0;
+        let mut end = nums.len() - 1;
+        while start <= end {
+            let val;
+            if nums[start].abs() <= nums[end].abs() {
+                val = nums[start];
+                start += 1;
+            } else {
+                val = nums[end];
+                end -= 1;
+            }
+            dp.push(val * val)
+        }
+        dp
+    }
+
+    pub fn fib(n: i32) -> i32 {
+        /* 斐波那契数
+        F(N) = F(N - 1) + F(N - 2)
+        输入：n = 5
+        输出：5
+        */
+        let n = n as usize;
+        let mut dp = [0, 1, 1];
+        if n <= 2 {
+            return dp[n];
+        }
+        for _i in 2..=n {
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = dp[0] + dp[1];
+        }
+        dp[1]
+    }
 }
